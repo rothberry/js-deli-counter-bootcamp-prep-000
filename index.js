c1c43e301e9571541de60ee14faceb300c121d35
Philip Roth <phil.roth077@gmail.com>
2019-05-31 00:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var katzDeli = []
-
 function takeANumber(katzDeli, name) {
   katzDeli.push('${name}')
   return('Welcome, ${name}. You are number ${katzDeli.length} in line')
 }
+
 function nowServing(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
