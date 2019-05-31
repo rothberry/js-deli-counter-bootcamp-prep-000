79fabc4d5193c0d7abbb90e852206ce9fd168f2c
Philip Roth <phil.roth077@gmail.com>
2019-05-31 00:41:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// function takeANumber(katzDeli, name) {
+//   katzDeli.push('${name}')
+//
+//   return ('Welcome, ${name}. You are number ${katzDeli.length} in line.')
+// }
+var line = 0
+function takeANumber(katzDeli) {
+  katzDeli.push(line++)
+
+  return ('You are number ${line} in line.')
+}
+
+function nowServing(katzDeli) {
+  let i = 0
+  while (i<katzDeli.length) {
+    i++
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return ('Currently serving ${katzDeli.shift()}.')
+  }
+}
+
+function currentLine(katzDeli) {
+  let i = 0
+  while (i<katzDeli.length) {
+    line++
+  }
+  if (katzDeli === 0) {
+    return "The line is currently empty."
+  } else {
+    return ('The line is currently:  ${line}')
+  }
+}
